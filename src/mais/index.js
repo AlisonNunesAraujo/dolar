3ed67b5c25e3f7619a0f199931811f735e772979
alisondev <araujoalison304@gmail.com>
2024-06-09 17:21:26 -0300

    'upload'

---
+++ src/mais/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View, Text} from 'react'
+
+function Mais(){
+    return(
+        <View>
+            <Text>olaa</Text>
+        </View>
+    )
+}
+
+export default Mais;
---
+++ src/sobre/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View, Text} from 'react'
+
+function Sobre(){
+    return(
+        <View>
+            <Text>olaa</Text>
+        </View>
+    )
+}
+
+export default Sobre;
